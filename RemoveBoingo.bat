b08974b6a80427526fce2a2ab88692c1bde6c5e9
B00TK1D <55107317+B00TK1D@users.noreply.github.com>
2021-12-04 15:53:15 -0700

    Update RemoveBoingo.bat

--- RemoveBoingo.bat
+++ RemoveBoingo.bat
@@ -1,8 +1,10 @@
 taskkill /im a1g.exe
-del /f C:\Windows\SysWOW64\kt
-del /f C:\Program Files\Common Files\a1g.exe
-del /f C:\Windows\SysWOW64\a1g.exe
-del /f C:\a1g.exe
-reg delete  "HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Run" /v "Analog Logic Gate Conversion"
+del /f "C:\Windows\SysWOW64\kt"
+del /f "C:\Program Files\Common Files\a1g.exe"
+del /f "C:\Program Files\Common Files\kt"
+del /f "C:\Windows\SysWOW64\a1g.exe"
+del /f "C:\a1g.exe"
+reg delete  "HKEY_LOCAL_MACHINE\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Run" /v /y "Analog Logic Gate Conversion"
 sc delete "Windows Updater Service"
 start /b "" cmd /c del "%~f0"&exit /b
+exit
